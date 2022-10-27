94b70ac380933a9af9a1b791d48d4c130b45a1c1
mizanor21 <mizanorrahman398@gmail.com>
2022-10-27 19:01:08 +0600

    React-to-pdf part  added

--- src/components/CourseDetails/CourseDetails.js
+++ src/components/CourseDetails/CourseDetails.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import Pdf from "react-to-pdf";
+
 import { Link, useLoaderData } from 'react-router-dom';
+const ref = React.createRef();
 
 const CourseDetails = () => {
     const course = useLoaderData();
     // console.log(course)
     return (
         <div className='flex justify-center container mx-auto my-10'>
-            <div className="">
+
+            <div ref={ref} className="">
                 <img className='rounded-lg w-full' src={course.img} alt="" />
-                <div className="">
+                <div className="flex justify-between my-5">
                     <h3 className='font-bold text-2xl'>{course.title}</h3>
-                    <Link></Link>
+                    <Pdf targetRef={ref} filename="code-example.pdf">
+                        {({ toPdf }) => <button className="btn btn-outline btn-sm btn-success" onClick={toPdf}>Download Pdf</button>}
+                    </Pdf>
                 </div>
                 <p>{course.details}</p>
                 <div className="text-end mt-10">
---
+++ src/components/ReactPdf/ReactPdf.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ReactPdf = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default ReactPdf;
